6c1869a17f2ad593372f0538b8a449229ddf6299
Psyrenq <gogodzilla101@gmail.com>
2023-01-28 11:55:29 -0500

    WDD230

--- Chamber/scripts/script.js
+++ Chamber/scripts/script.js
@@ -1,7 +1,17 @@
-var lastUpdated = `Last updated: ${document.lastModified}`;
+var lastUpdated = `Last Modification: ${document.lastModified}`;
     document.querySelector("#updateDate").innerText=lastUpdated;
  
 const currentDate = document.querySelector("#currentYear");
     currentDate.textContent = `${new Date().getFullYear()}`;
     //It's working, so I'm assuming this is the correct method?
+
+//function toggleMenu() {
+//    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+//}
+
+
+
+const dateField = document.querySelector(".current");
+const fullDate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(date);
+dateField.innerHTML = fullDate;
 //©-River-Rhodes
