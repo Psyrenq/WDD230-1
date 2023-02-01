eb8f7b7ccfca9ece53716167279e08fa35ea4f0c
Psyrenq <gogodzilla101@gmail.com>
2023-02-01 18:58:39 -0500

    WDD230

--- Chamber/scripts/script.js
+++ Chamber/scripts/script.js
@@ -1,3 +1,13 @@
+//Menu
+function toggleMenu(){
+    document.getElementById("navigation").classList.toggle("open");
+    document.getElementById("hambButton").classList.toggle("open");
+
+}
+ const x = document.getElementById("menuButton");
+ x.onclick = toggleMenu;
+
+//date
 var lastUpdated = `Last Modification: ${document.lastModified}`;
     document.querySelector("#updateDate").innerText=lastUpdated;
  
@@ -5,13 +15,8 @@ const currentDate = document.querySelector("#currentYear");
     currentDate.textContent = `${new Date().getFullYear()}`;
     //It's working, so I'm assuming this is the correct method?
 
-//function toggleMenu() {
-//    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-//}
-
-
-
-const dateField = document.querySelector(".current");
-const fullDate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(date);
-dateField.innerHTML = fullDate;
+//current date
+const now = new Date();
+const fullDate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(now);
+ document.querySelector(".date").innerHTML = fullDate;
 //©-River-Rhodes
