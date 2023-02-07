3296e034dfb82744632469d4f8c62bd061b15d88
Psyrenq <gogodzilla101@gmail.com>
2023-02-07 17:16:50 -0500

    WDD230

---
+++ Chamber/scripts/date.js
@@ -0,0 +1,32 @@
+let myDate = new Date();
+
+let weekDay = new Array(7);
+weekDay[0] = "Sunday";
+weekDay[1] = "Monday";
+weekDay[2] = "Tuesday";
+weekDay[3] = "Wednesday";
+weekDay[4] = "Thursday";
+weekDay[5] = "Friday";
+weekDay[6] = "Saturday";
+
+let month = new Array(12);
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+
+const meetingBanner = document.querySelector(".banner");
+window.addEventListener("load", () => {
+  if (myDate.getDay() < 3 && 
+    myDate.getDay() > 0) {
+    meetingBanner.style.display = "block";
+  }
+}, false);
