2781d00ca9ea988564c7f7cb56b0ba769e44f4e2
Psyrenq <gogodzilla101@gmail.com>
2023-02-26 21:40:50 -0500

    WDD230

--- Chamber/scripts/visits.js
+++ Chamber/scripts/visits.js
@@ -5,12 +5,9 @@ let numVisits = Number(window.localStorage.getItem("visits-ls"));
 
 if (numVisits !== 0) {
 	visitsDisplay.textContent = numVisits;
-} else {
-	visitsDisplay.textContent = `Welcome to your first time on this page!`;
 }
 
 numVisits++;
 localStorage.setItem("visits-ls", numVisits);
 todayDisplay.textContent = Date.now();
-let longnum= 1676650639658;
 let today = new Date();
